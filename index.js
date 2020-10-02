2f8be3b9411fa4d5cb8988dca5813e26b965a9d4
Tony Rivas <tonyjrivas@gmail.com>
2020-10-02 15:44:51 -0600

    updated js

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 // array of questions for user
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 const questions = [
+    {
+        type: "input",
+        name: "title", 
+        message: "What would you like the title of your project to be?",
+
+    },
+    {
+        type: "input", 
+        name: "description",
+        message: "Describe your project"
+    },
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+    .then(function(response){
+        writeToFile("README.md", generateMarkdown({
+            ...response
+        }));
+    });
 }
 
 // function call to initialize program
